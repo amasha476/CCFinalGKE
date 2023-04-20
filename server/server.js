807b2b384b036c6028be17a1ece1c98fef0c08cd
amasha476 <66602169+amasha476@users.noreply.github.com>
2023-04-20 19:55:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,19 +11,19 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-const Schema = mongoose.Schema;
-const responseSchema = new Schema({
-    prompt: { type: String },
-    status: { type: String },
-    created: { type: Number },
-    message: { type: String },
-    total_tokens: { type: Number }
-});
-const Response = mongoose.model("Response", responseSchema);
+// const Schema = mongoose.Schema;
+// const responseSchema = new Schema({
+//     prompt: { type: String },
+//     status: { type: String },
+//     created: { type: Number },
+//     message: { type: String },
+//     total_tokens: { type: Number }
+// });
+// const Response = mongoose.model("Response", responseSchema);
 
 const createAndSaveResponse = (apiResponse) => {
     return apiResponse.save()
